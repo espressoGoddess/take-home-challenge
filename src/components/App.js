dfc5e75545ff0dbd3165b785876cfbcd65bf8e29
Amber Shipley <ashipley2@me.com>
2023-06-02 08:33:47 -0700

    Fix a bug on 404 page (go back button wasn't showing)

--- src/components/App.js
+++ src/components/App.js
@@ -77,7 +77,7 @@ export default function App() {
               <Route path='/articles'>
                 <Search setSearchTerm={setSearchTerm}/>
               </Route>
-              <Route path='/'>
+              <Route exact path='/'>
                 {null}
               </Route>
               <Route path='*'>
